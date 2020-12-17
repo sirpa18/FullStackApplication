17f8606bb27ec5679049cd72bfbd2e42d14e29a6
sirpa18 <51848256+sirpa18@users.noreply.github.com>
2020-12-17 13:23:25 -0600

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from config import Config
+from flask_mongoengine import MongoEngine
+from flask_restx import Api,api
+from werkzeug.utils import *
+#from functools import cached_property
+
+api = Api()
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = MongoEngine()
+db.init_app(app)
+api.init_app(app)
+
+from application import routes
+
